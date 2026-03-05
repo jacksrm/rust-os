31ae6120394c6c5788557990b0bcd88d79f3f3fb
Jacson Rodrigues <github.obsessive123@passmail.net>
2026-03-05 16:24:12 -0300

    First implementaion of VGA Buffer Memory

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@
 
 use core::panic::PanicInfo;
 
+static _HELLO: &[u8] = b"Hello World";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
     loop {}
 }
 
@@ -12,3 +16,5 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+mod vga_buffer;
