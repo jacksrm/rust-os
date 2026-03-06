ea3fd22088dde9896b999bdccef6d4c7e81357ff
Jacson Rodrigues <github.obsessive123@passmail.net>
2026-03-06 00:07:58 -0300

    Updates panic! macro to use println! macro

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ static _HELLO: &[u8] = b"Hello World";
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    panic!("Something went wrong!");
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
