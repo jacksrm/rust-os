fe8655dca5615c151315a054650f24aedcdf34a9
Jacson Rodrigues <github.obsessive123@passmail.net>
2026-03-06 06:24:47 -0300

    Ads the arguments to use isa-debug-exit on QUEMU

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -8,7 +11,10 @@ static _HELLO: &[u8] = b"Hello World";
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Something went wrong!");
+    // panic!("Something went wrong!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -20,3 +26,18 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 mod vga_buffer;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("Trivial Assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
